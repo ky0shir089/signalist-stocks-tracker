9ecc02d78c1c429ad2df43bd73cca005aded4f79
ky0shir089 <ky0shir0_13@yahoo.co.id>
2025-10-08 16:41:37 +0700

    implement Auth UI & Layout

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,92 @@
+"use client"
+
+import {zodResolver} from "@hookform/resolvers/zod";
+import {useForm} from "react-hook-form"
+import {z} from "zod";
+
+import {Button} from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import {Input} from "@/components/ui/input"
+import {useTransition} from "react";
+import {PasswordInput} from "@/components/ui/password-input";
+import FooterLink from "@/components/FooterLink";
+
+const SignInPage = () => {
+    const [isSubmitting, startTransition] = useTransition()
+
+    const formSchema = z.object({
+        email: z.email(),
+        password: z.string().min(8),
+    })
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+    })
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        startTransition(async () => {
+            try {
+                console.log(values)
+            } catch (error) {
+                console.log(error)
+            }
+        })
+    }
+
+    return (
+        <>
+            <h1 className="form-title">Welcome Back</h1>
+
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                    <FormField
+                        control={form.control}
+                        name="email"
+                        render={({field}) => (
+                            <FormItem>
+                                <FormLabel>Email</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="Email" {...field} required/>
+                                </FormControl>
+                                <FormMessage/>
+                            </FormItem>
+                        )}
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="password"
+                        render={({field}) => (
+                            <FormItem>
+                                <FormLabel>Password</FormLabel>
+                                <FormControl>
+                                    <PasswordInput placeholder="Password" {...field} />
+                                </FormControl>
+                                <FormMessage/>
+                            </FormItem>
+                        )}
+                    />
+
+
+                    <Button type="submit" disabled={isSubmitting} className="yellow-btn w-full mt-5">
+                        {isSubmitting ? "Signing In" : "Sign In"}
+                    </Button>
+
+                    <FooterLink text="Don't have an Account?" linkText="Create An Account" href="/sign-in"/>
+                </form>
+            </Form>
+        </>
+    )
+}
+export default SignInPage
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,192 @@
+"use client"
+
+import {zodResolver} from "@hookform/resolvers/zod";
+import {useForm} from "react-hook-form"
+import {z} from "zod";
+
+import {Button} from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import {Input} from "@/components/ui/input"
+import {useTransition} from "react";
+import {PasswordInput} from "@/components/ui/password-input";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+import {INVESTMENT_GOALS, PREFERRED_INDUSTRIES, RISK_TOLERANCE_OPTIONS} from "@/lib/constants";
+import FooterLink from "@/components/FooterLink";
+import {CountrySelectField} from "@/components/forms/CountrySelectField";
+
+const SignUpPage = () => {
+    const [isSubmitting, startTransition] = useTransition()
+
+    const formSchema = z.object({
+        fullName: z.string().min(1),
+        email: z.email(),
+        password: z.string().min(8),
+        country: z.string().min(2),
+        investmentGoals: z.string().min(1),
+        riskTolerance: z.string().min(1),
+        preferredIndustry: z.string().min(1),
+    })
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            fullName: "",
+            email: "",
+            password: "",
+            country: "ID",
+            investmentGoals: "Growth",
+            riskTolerance: "Medium",
+            preferredIndustry: "Technology"
+        },
+    })
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        startTransition(async () => {
+            try {
+                console.log(values)
+            } catch (error) {
+                console.log(error)
+            }
+        })
+    }
+
+    return (
+        <>
+            <h1 className="form-title">Sign Up and Personalize</h1>
+
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                    <FormField
+                        control={form.control}
+                        name="fullName"
+                        render={({field}) => (
+                            <FormItem>
+                                <FormLabel>Full Name</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="Full Name" {...field} required/>
+                                </FormControl>
+                                <FormMessage/>
+                            </FormItem>
+                        )}
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="email"
+                        render={({field}) => (
+                            <FormItem>
+                                <FormLabel>Email</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="Email" {...field} required/>
+                                </FormControl>
+                                <FormMessage/>
+                            </FormItem>
+                        )}
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="password"
+                        render={({field}) => (
+                            <FormItem>
+                                <FormLabel>Password</FormLabel>
+                                <FormControl>
+                                    <PasswordInput placeholder="Password" {...field} />
+                                </FormControl>
+                                <FormMessage/>
+                            </FormItem>
+                        )}
+                    />
+
+                    <CountrySelectField name="country" label="country" control={form.control}/>
+
+                    <FormField
+                        control={form.control}
+                        name="investmentGoals"
+                        render={({field}) => (
+                            <FormItem>
+                                <FormLabel>Investment Goals</FormLabel>
+                                <Select onValueChange={field.onChange} defaultValue={field.value}>
+                                    <FormControl>
+                                        <SelectTrigger className="w-full">
+                                            <SelectValue placeholder="Select an Investment Goals"/>
+                                        </SelectTrigger>
+                                    </FormControl>
+                                    <SelectContent>
+                                        {INVESTMENT_GOALS.map(item => <SelectItem key={item.value}
+                                                                                  value={item.value}>{item.value}</SelectItem>)}
+                                    </SelectContent>
+                                </Select>
+                                <FormMessage/>
+                            </FormItem>
+                        )}
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="riskTolerance"
+                        render={({field}) => (
+                            <FormItem>
+                                <FormLabel>Risk Tolerance</FormLabel>
+                                <Select onValueChange={field.onChange} defaultValue={field.value}>
+                                    <FormControl>
+                                        <SelectTrigger className="w-full">
+                                            <SelectValue placeholder="Select your Risk Level"/>
+                                        </SelectTrigger>
+                                    </FormControl>
+                                    <SelectContent>
+                                        {RISK_TOLERANCE_OPTIONS.map(item => <SelectItem key={item.value}
+                                                                                        value={item.value}>{item.value}</SelectItem>)}
+                                    </SelectContent>
+                                </Select>
+                                <FormMessage/>
+                            </FormItem>
+                        )}
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="preferredIndustry"
+                        render={({field}) => (
+                            <FormItem>
+                                <FormLabel>Preferred Industry</FormLabel>
+                                <Select onValueChange={field.onChange} defaultValue={field.value}>
+                                    <FormControl>
+                                        <SelectTrigger className="w-full">
+                                            <SelectValue placeholder="Select your Preferred Industry"/>
+                                        </SelectTrigger>
+                                    </FormControl>
+                                    <SelectContent>
+                                        {PREFERRED_INDUSTRIES.map(item => <SelectItem key={item.value}
+                                                                                      value={item.value}>{item.value}</SelectItem>)}
+                                    </SelectContent>
+                                </Select>
+                                <FormMessage/>
+                            </FormItem>
+                        )}
+                    />
+
+                    <Button type="submit" disabled={isSubmitting} className="yellow-btn w-full mt-5">
+                        {isSubmitting ? "Creating Account" : "Start Your Investing Journey"}
+                    </Button>
+
+                    <FooterLink text="Already have an Account?" linkText="Sign In" href="/sign-in"/>
+                </form>
+            </Form>
+        </>
+    )
+}
+export default SignUpPage
